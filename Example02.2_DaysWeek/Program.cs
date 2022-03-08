f1c153db088c157fcb56edd811b5150c45056ad5
Nikolay <kolya_fedosov1992@example.com>
2022-03-08 16:54:58 +0300

    add Example02.2 Week

---
+++ Example02.2_DaysWeek/Program.cs
@@ -0,0 +1,44 @@
+﻿int days = new int();
+Console.WriteLine("Ведите число");
+days = Convert.ToInt32(Console.ReadLine());
+
+switch (days)
+{
+    case 1:
+        {
+            Console.WriteLine("Понедельник");
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Вторник");
+            break;
+        }
+    case 3:
+        {
+            Console.WriteLine("Среда");
+            break;
+        }
+    case 4:
+        {
+            Console.WriteLine("Четверг");
+            break;
+        }    
+    case 5:
+    {
+        Console.WriteLine("Пятница");
+        break;
+    }
+    case 6:
+    {
+        Console.WriteLine("Субота");
+        break;
+    }
+    case 7:
+    {
+        Console.WriteLine("Воскресенье");
+        break;
+    }    
+}
+
+Console.WriteLine(days);
