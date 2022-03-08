112903d67d2c62565a02e9bd71055e4f27ffeb3e
Nikolay <kolya_fedosov1992@example.com>
2022-03-08 17:11:53 +0300

    add Example02.3 True and False

---
+++ Exemple02.1_TrueFalse/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ведите число №1");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите число №2");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number2 == number1 * number1)
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("False");
+}
