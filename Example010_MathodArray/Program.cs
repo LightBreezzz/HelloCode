3b462407007057ba8d52a2be924d3ee9ca466d5c
Nikolay <kolya_fedosov1992@example.com>
2022-03-11 13:08:14 +0300

    add Example 010 Mathod Array

---
+++ Example010_MathodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 41, 27, 85, 32, 4, 45, 65, 67, 73, 85 };
+
+int n = array.Length;
+int find = 85;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++;
+}
