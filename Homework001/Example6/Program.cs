d436ea765dee62ce6d32737ca0008b382dbbef0f
Nikolay <kolya_fedosov1992@example.com>
2022-03-15 14:30:05 +0300

    add Example 6 A%2==0

---
+++ Homework001/Example6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Ведите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Можно делить на 2?");
+
+if ( A % 2 == 0 )
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
