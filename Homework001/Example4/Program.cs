ff803e3fb19fff29367f624507386f273d0e8d18
Nikolay <kolya_fedosov1992@example.com>
2022-03-15 14:05:17 +0300

    add Example 4 Max in 3

---
+++ Homework001/Example4/Program.cs
@@ -0,0 +1,14 @@
+﻿int A = new Random().Next(1,10);
+int B = new Random().Next(1,10);
+int C = new Random().Next(1,10);
+
+Console.Write( A );
+Console.Write( B );
+Console.WriteLine( C );
+
+int max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.Write( "max = " );
+Console.Write( max );
