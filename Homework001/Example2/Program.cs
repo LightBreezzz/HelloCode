94a80d6a9478bc92bdbe412c44f3474ef8ce5fc6
Nikolay <kolya_fedosov1992@example.com>
2022-03-15 13:53:40 +0300

    add Example 2 Max and Min

---
+++ Homework001/Example2/Program.cs
@@ -0,0 +1,13 @@
+﻿int A = 5;
+int B = 2;
+
+int max = A;
+int min = B;
+
+if ( B > max ) max = B;
+if ( A < min ) min = A;
+
+Console.Write("min = ");
+Console.WriteLine( min );
+Console.Write("max = ");
+Console.WriteLine( max );
