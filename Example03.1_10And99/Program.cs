03c0903804472404a08991760cfdb6b5ba69ca4b
Nikolay <kolya_fedosov1992@example.com>
2022-03-17 17:03:38 +0300

    add Example 3.1 10 and 99

---
+++ Example03.1_10And99/Program.cs
@@ -0,0 +1,18 @@
+﻿int GetMaxLetterOfNumber(int number)
+{
+    int result = number % 10;
+    int letter2 = number / 10;
+    if (result < letter2)
+    {
+        result = letter2;
+    }
+    return result;
+}
+
+
+int randNumber = new Random().Next(10, 100);
+int result = GetMaxLetterOfNumber(randNumber);
+Console.WriteLine($"у числа {randNumber} наибольшая цифра {result}");
+
+//Console.WriteLine($"у числа " + rendNumber + " наибольшая цифра " + result");
+
