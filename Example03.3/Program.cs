c79155c01e32225afa56d6188ebeb75ebc075a9f
Nikolay <kolya_fedosov1992@example.com>
2022-03-27 19:28:52 +0300

    add Example 3.3 True and False

---
+++ Example03.3/Program.cs
@@ -0,0 +1,15 @@
+﻿int Number = new int();
+System.Console.WriteLine("Введите число");
+Number = Convert.ToInt32(Console.ReadLine());
+
+int Number1 = Number / 10;
+int Number2 = Number % 10;
+
+if (Number1 % Number2 == 0)
+    {
+        Console.WriteLine("Trye");
+    }
+else
+    {
+        Console.WriteLine("False");
+    }
