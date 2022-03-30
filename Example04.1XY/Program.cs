026668aa47b442dccf7348aa561177ceba7dcf79
Nikolay <kolya_fedosov1992@example.com>
2022-03-30 21:04:34 +0300

    add Example 4.1 X and Y

---
+++ Example04.1XY/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите x and y");
+
+int X = Convert.ToInt32(Console.ReadLine());
+int Y = Convert.ToInt32(Console.ReadLine());
+
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("Первая чктвердь");
+}
+else if (X > 0 && Y < 0)
+{
+    Console.WriteLine("Вторая чктвердь");
+}
+else if (X < 0 && Y < 0)
+{
+    Console.WriteLine("Третья чктвердь");
+}
+else if (X < 0 && Y > 0)
+{
+    Console.WriteLine("Четвертая чктвердь");
+}
