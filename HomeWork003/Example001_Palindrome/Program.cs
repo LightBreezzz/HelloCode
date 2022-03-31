e5864927b21488cb20469edb6affc5cba5b74a43
Nikolay <kolya_fedosov1992@example.com>
2022-03-31 11:18:17 +0300

    add HW Example1 V2

--- HomeWork003/Example001_Palindrome/Program.cs
+++ HomeWork003/Example001_Palindrome/Program.cs
@@ -1,4 +1,41 @@
-﻿int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
+﻿int reversNumber(int number)
+{
+    int reversNumber = new int();
+
+    while (number > 0)
+    {
+        int lastDigit = number % 10;
+        reversNumber = reversNumber * 10 + lastDigit;
+        number /= 10;
+    }
+    return reversNumber;
+}
+
+string InNumberPolindrom(int number)
+{
+    string returnMassage = $"{number}";
+
+    int reversedNumber = reversNumber(number);
+    if (reversedNumber == number)
+    {
+        returnMassage += (" - это полиндром");
+    }
+    else
+    {
+        returnMassage += (" - это не полиндром");
+    }
+    return returnMassage;
+}
+
+int number = Convert.ToInt32(Console.ReadLine());
+string returnMassage = InNumberPolindrom(number);
+System.Console.WriteLine(returnMassage);
+
+
+
+
+
+/*int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
 string returnMassage = $"{fiveDigitNumber}";
 if (fiveDigitNumber > 9999 && fiveDigitNumber < 100000)
 {
@@ -22,3 +59,5 @@ else
         returnMassage += (" - это не пятизначное число");
     }
     System.Console.WriteLine(returnMassage);
+int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
+*/
