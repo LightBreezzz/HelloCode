b56799b84553dfa8441ff8dc7ca75236c55c5901
Nikolay <kolya_fedosov1992@example.com>
2022-03-31 12:33:55 +0300

    add Example 5.2 26 Get Number Of Digit

---
+++ Example05.2_26/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumberOfDigit(int number)
+{
+    int NumberOfDigit = 0;
+    while (number > 0)
+    {
+        number /= 10;
+        NumberOfDigit++;
+        Console.WriteLine(number);
+    }
+    return NumberOfDigit;
+}
+
+while (true)
+{
+    Console.WriteLine("Ведите число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    int numberOfDigit = GetNumberOfDigit(number);
+    Console.WriteLine($"колличество цифр {numberOfDigit}");
+}
