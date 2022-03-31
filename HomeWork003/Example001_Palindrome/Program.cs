47741ab19a1329ab5bab92771c55ee7ef2b83792
Nikolay <kolya_fedosov1992@example.com>
2022-03-31 10:28:38 +0300

    add HomeWork003 Example01 Palendrome

---
+++ HomeWork003/Example001_Palindrome/Program.cs
@@ -0,0 +1,24 @@
+﻿int fiveDigitNumber = Convert.ToInt32(Console.ReadLine());
+string returnMassage = $"{fiveDigitNumber}";
+if (fiveDigitNumber > 9999 && fiveDigitNumber < 100000)
+{
+    int[] digit = new int[5];
+    for (int i = 0; i < digit.Length; i++)
+    {
+        digit[i] = fiveDigitNumber % 10;
+        fiveDigitNumber /= 10;
+    }
+    if(digit[0] == digit[4] && digit[1] == digit[3])
+    {
+        returnMassage += (" - это полиндром");
+    }
+    else
+    {
+        returnMassage += (" - это не полиндром");
+    }
+}
+else
+    {
+        returnMassage += (" - это не пятизначное число");
+    }
+    System.Console.WriteLine(returnMassage);
