2209f1c89b24f625682ac700a93088a39cea733b
Nikolay <kolya_fedosov1992@example.com>
2022-03-31 11:54:26 +0300

    add Example 5.1.24 Sum do number

---
+++ Example05.1_24SumDoA/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Ведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int count = 0;
+while (count <= number)
+{
+    result += count;
+    count++;
+}
+System.Console.WriteLine(result);
