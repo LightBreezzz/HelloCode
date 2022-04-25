3ea6bda9bab3613d95f6a6da7ec113da0262b4dd
Nikolay <kolya_fedosov1992@example.com>
2022-04-25 15:09:00 +0300

    add Example 06.3 Find Number

---
+++ Example06.3_FindNumber/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] GetArrayOfNumber(int length, int deviation)
+{
+    int [] arrayOfNumbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arrayOfNumbers[i] = new Random().Next(-deviation,deviation+1);
+    }
+    return arrayOfNumbers;
+}
+
+bool IsNumberInArray(int number, int[] arrayOfNumbers)
+{
+    bool isInArray = false;
+    foreach (var item in arrayOfNumbers)
+    {
+        if(item == number)
+        {
+            isInArray = true;
+            break;
+        }
+        else
+        {
+            isInArray = false;
+        }
+    }
+    return isInArray;
+}
+
+void printArray(int[] arrayOfNumbers)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayOfNumbers.Length; i++)
+    {   
+        Console.Write(arrayOfNumbers[i]);
+        if (i != arrayOfNumbers.Length -1)
+        {
+            Console.Write(",");
+        }   
+    } 
+    Console.WriteLine("]");
+}
+
+int[] arrayOfNumbers = GetArrayOfNumber(5, 100);
+System.Console.Write("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+printArray(arrayOfNumbers);
+bool isInArray = IsNumberInArray(number, arrayOfNumbers);
+Console.WriteLine(isInArray);
