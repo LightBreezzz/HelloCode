89824b5cabcbe55f969fc4ea69c0d2f6e2f1b41e
Nikolay <kolya_fedosov1992@example.com>
2022-05-17 01:15:33 +0300

    add Example 13 HomeWork

---
+++ HomeWork002/Example13/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+int GetTreeNumbersOfNumber(int number)
+{
+    int count = number;
+    System.Console.WriteLine(number);
+
+    while (count > 999)
+    {
+        count /= 10;
+    }
+    return count;
+}
+int GetTreeNumberOfNumber(int count)
+{
+        if (count > 99)
+        {
+            count %= 10;
+            Console.Write(count);
+        }
+        else
+        {
+            Console.Write("No");
+        }
+        return count;
+}
+
+int randnumber = new Random().Next(1, 9999);
+int count = GetTreeNumbersOfNumber(randnumber);
+int result = GetTreeNumberOfNumber(count);
+Console.WriteLine($" third number out of {randnumber} " );
