9e339821c07cdecfe70b4c9188046dec479ba369
Nikolay <kolya_fedosov1992@example.com>
2022-05-17 01:27:42 +0300

    add Example 15

---
+++ HomeWork002/Example15/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+int days = new int();
+Console.WriteLine("Ведите число");
+days = Convert.ToInt32(Console.ReadLine());
+
+switch (days)
+{
+    case 1:
+        {
+            Console.Write($"{days} -> ");
+            Console.WriteLine("No");
+            break;
+        }
+    case 2:
+        {
+            Console.Write($"{days} -> ");
+            Console.WriteLine("No");
+            break;
+        }
+    case 3:
+        {
+            Console.Write($"{days} -> ");
+            Console.WriteLine("No");
+            break;
+        }
+    case 4:
+        {
+            Console.Write($"{days} -> ");
+            Console.WriteLine("No");
+            break;
+        }    
+    case 5:
+    {
+        Console.Write($"{days} -> ");
+        Console.WriteLine("No");
+        break;
+    }
+    case 6:
+    {
+        Console.Write($"{days} -> ");
+        Console.WriteLine("Yas");
+        break;
+    }
+    case 7:
+    {
+        Console.Write($"{days} -> ");
+        Console.WriteLine("Yas");
+        break;
+    }    
+}
