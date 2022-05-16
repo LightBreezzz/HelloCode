5e2cf6aa12b28bd5c35f830f3c49671e99efa00a
Nikolay <kolya_fedosov1992@example.com>
2022-05-16 22:53:13 +0300

    add Example 10 HomeWork

---
+++ HomeWork002/Example10/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+int Number = new Random().Next(100, 1000);
+Console.WriteLine(Number);
+
+int result = Number / 10;
+//Console.WriteLine(result);
+
+result %= 10;
+Console.WriteLine($"{result} is the sekond number out of {Number}");
