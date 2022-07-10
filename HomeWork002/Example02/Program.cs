22595a6e3825aad274a2010b4112952ff0f35ba5
Nikolay <kolya_fedosov1992@example.com>
2022-07-10 20:43:20 +0300

    add Example02 HomeWork

---
+++ HomeWork002/Example02/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+    Напишите программу, которая выводит
+    случайное трёхзначное число и удаляет вторую цифру
+    этого числа.
+*/
+
+int GetNumber(int number)
+{
+    int fest = number;
+    int Last = number;
+    Console.WriteLine(number);
+
+    while (fest < 999 & Last > 99)
+    {
+        Last /= 100; 
+        fest %= 10;
+        Console.Write(Last);
+        Console.WriteLine(fest);
+    }
+    return Last;
+}
+
+int randnumber = new Random().Next(100, 1000);
+int count = GetNumber(randnumber);
