69ddae7bf0fba63d4313af7bbd0c82b59d8f8fcf
Nikolay <kolya_fedosov1992@example.com>
2022-07-28 14:33:11 +0300

    add HomeWork 4 #25

--- Example02.1_Task0/Program.cs
+++ Example02.1_Task0/Program.cs
@@ -1,7 +1,64 @@
-﻿int number = new int();
-Console.WriteLine("Ведите число");
-number = Convert.ToInt32(Console.ReadLine());
+﻿
+var totalCount = 0;
+string[] elements = { "Rem", "Hisoka", "Gon", "Monkey D Luffy", "Alvida", "Shank" };
+totalCount = elements.Count();
+//Console.WriteLine(totalCount);
 
-number = number * number;
 
-Console.WriteLine(number);
+string Array(string[] elements)
+{
+    //string[] arr = {"32fg", "fb", "d=4", "dfgg"};
+    string result = String.Empty;
+    for (int i = 0; i < elements.Length; i++)
+    {
+        string count = elements[i];
+        result = result + count;
+        Console.WriteLine(result);
+        result = String.Empty;
+    }
+    return result;
+}
+
+ Array(elements);
+Console.WriteLine(Array);
+
+/*string Method4(int count, string Text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+         result = result + Text;
+         i++;
+    }
+    return result;
+}
+ string res = Method4(totalCount, text);
+Console.WriteLine(res);
+
+
+
+string Method(string elements)
+{
+        string result = String.Empty;
+    for(int i = 0; i < totalCount; i++)
+    {
+        result = result + elements;
+        System.Console.WriteLine(result);
+    }
+}
+
+string[] count = Method(elements);
+
+
+string Method4(int totalCount, string elements)
+{
+    string result = String.Empty;
+    for(int i = 0; i < totalCount; i++)
+    {
+        result = result + elements;
+        System.Console.WriteLine(result);
+
+    }
+    return result;
+}*/
---
+++ HomeWork004/Example25/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Задача 25
+    Напиши цикл, который принимает на вход два числа (А и В) 
+    и возводит число А в натуральную степень В.
+    3,5 -> 243(3^5)
+    2,4 -> 16
+*/
+
+Console.Write("Введите число А - ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число В - ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+int GetNumberInDegree(int numberA, int numberB)
+{
+    int i = 1;
+    int result = 1;
+    while (i <= numberB)
+    {
+        result *= numberA;
+        i ++;
+    }
+    return result;
+}
+int result = GetNumberInDegree(numberA, numberB);
+Console.WriteLine(result);
