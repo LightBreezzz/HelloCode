fcf12943fe65d6ee525081eade71ce579af34fea
Nikolay <kolya_fedosov1992@example.com>
2022-09-01 14:20:19 +0300

    add HomeWork Ex27

---
+++ HomeWork004/Example27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напиши программу, которая принимает на вход число и выдает сумму в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число - ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumNumber(int number)
+{
+    int Numbers = Convert.ToString(number).Length;
+    int  count = 0;
+    int result = 0;
+    for (int i = 0; i < Numbers; i++)
+    {
+        count = number - number % 10;
+        result = result + (number - count);
+        number = number / 10;
+    }
+    return result;
+}
+int result = SumNumber(number);
+Console.WriteLine($"Сумма цифр в числе - {result}");
