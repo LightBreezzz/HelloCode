04e30951acbbee048b04762db65d2e6a21ca8b54
Nikolay <kolya_fedosov1992@example.com>
2022-09-05 15:26:38 +0300

    add HomeWork04  Example29

---
+++ HomeWork004/Example29/Program.cs
@@ -0,0 +1,48 @@
+﻿/*
+ Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+ 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+ 6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] GetArrayOfNumber(int length)
+{
+    int[] arrayOfNumbers = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        arrayOfNumbers[i] = new Random().Next(1, 50);
+    }
+    return arrayOfNumbers;
+}
+
+void printNumbers(int[] arrayOfNumbers)
+{
+    for (int i = 0; i < arrayOfNumbers.Length; i++)
+    {
+        Console.Write(arrayOfNumbers[i]);
+        if (i != arrayOfNumbers.Length -1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write(" -> ");
+}
+
+
+void printArray(int[] arrayOfNumbers)
+{
+    Console.Write("[");
+    for (int i = 0; i < arrayOfNumbers.Length; i++)
+    {
+        Console.Write(arrayOfNumbers[i]);
+        if (i != arrayOfNumbers.Length -1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+int[] array = GetArrayOfNumber(8);
+
+printNumbers(array);
+printArray(array);
