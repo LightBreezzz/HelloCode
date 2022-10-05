198d87ed53f1c76340ac4273628c85118e9ff8ae
Nikolay <kolya_fedosov1992@example.com>
2022-10-05 17:17:03 +0300

    add HomeWork Example38

---
+++ HomeWork05/Example38/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3 7 22 2 78] -> 76
+*/
+
+double[] array = new double[5];
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    Console.Write(array[i] + " ");
+}
+
+double maxnumber = array[0];
+double minnumber = array[0];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (maxnumber < array[i])
+        {
+            maxnumber = array[i];
+        }
+
+        if (minnumber > array[i])
+        {
+            minnumber = array[i];
+        }
+    }
+
+double decision = maxnumber - minnumber;
+
+
+Console.WriteLine($"Разница между максимальным {maxnumber} и минимальным {minnumber} элементом: {decision}");
